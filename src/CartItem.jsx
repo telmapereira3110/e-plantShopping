fd6a5d42cc94a44ab37c76312f40803d96a4b535
Telma Pereira <telmapereira7@outlook.com>
2025-07-08 15:13:28 +0100

    Alterações feitas com a segunda parte do projeto

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -8,28 +8,51 @@ const CartItem = ({ onContinueShopping }) => {
   const dispatch = useDispatch();
 
   // Calculate total amount for all products in the cart
-  const calculateTotalAmount = () => {
- 
+  const calculateTotalAmount = (cart) => {
+    let total = 0;
+    cart.forEach(item => {
+      const quantity = item.quantity;
+      const costNumber = parseFloat(item.cost.substring(1));
+      total += costNumber * quantity
+    });
+
+    return total;
+
   };
 
   const handleContinueShopping = (e) => {
-   
+    onContinueShopping(e);
+  };
+
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
   };
 
 
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+    if (item.quantity > 1) {
+      dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
+    } else {
+      dispatch(removeItem(item.name))
+    }
+    
   };
 
   const handleRemove = (item) => {
+    dispatch(removeItem(item.name))
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+    const unitPrice = parseFloat(item.cost.substring(1));
+    const subtotal = unitPrice * item.quantity;
+    return subtotal;
+
   };
 
   return (
